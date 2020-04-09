5d53c77e96938c7a91c52666921eb28f66c611a9
Rohil Tuli <rohil.tuli@gmail.com>
2020-04-09 12:12:26 -0400

    Finished day 2

--- day2.hs
+++ day2.hs
@@ -2,8 +2,8 @@ import Data.Array
 
 main = do
    input <- readFile "day2.in"
-   print $ (runProgram (changeInitial (formArray input)) 0) ! 0
-
+   -- print $ (runProgram (changeInitial (formArray input) 12 2) 0) ! 0
+   print $ filterOutput $ possibleInputs $ formArray input
 
 formList :: String -> [Int]
 formList str = case break (==',') str of
@@ -14,8 +14,8 @@ formArray :: String -> Array Int Int
 formArray str = array (0, length list - 1) (zip [0,1..(length list)] list)
    where list = formList str
 
-changeInitial :: Array Int Int -> Array Int Int
-changeInitial arr = arr//[(1, 12), (2, 2)]
+changeInitial :: Array Int Int -> Int -> Int -> Array Int Int
+changeInitial arr x y = arr//[(1, x), (2, y)]
 
 runProgram :: Array Int Int -> Int -> Array Int Int
 runProgram arr x = 
@@ -23,3 +23,10 @@ runProgram arr x =
       1 -> runProgram (arr//[(arr ! (x+3), arr ! (arr ! (x+1)) + arr ! (arr ! (x+2)))]) (x+4)
       2 -> runProgram (arr//[(arr ! (x+3), arr ! (arr ! (x+1)) * arr ! (arr ! (x+2)))]) (x+4)
       99 -> arr
+
+possibleInputs :: Array Int Int -> [(Int, Int, Int)]
+possibleInputs arr = [(x, y, (runProgram (changeInitial arr x y) 0) ! 0) | x <- [0..99], y <- [0..99]]
+
+filterOutput :: [(Int, Int, Int)] -> Int
+filterOutput ((x, y, 19690720):_) = 100*x + y
+filterOutput ((_, _, _):tl) = filterOutput tl
