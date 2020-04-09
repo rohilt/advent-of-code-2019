f0ca1168673115bfc5d566e255ffafc2eeac2623
Rohil Tuli <rohil.tuli@gmail.com>
2020-04-09 16:02:48 -0400

    Finished day 3 part 1

--- day3.hs
+++ day3.hs
@@ -1,4 +1,5 @@
 import Data.List
+import qualified Data.Set as Set
 
 main = do
    input <- readFile "day3.in"
@@ -17,16 +18,16 @@ getPath list str =
    let (x, y) = endOfPath list in
    case (str) of
       ('R':tl) -> list ++ [(i, y) | i <- [x+1..(x+(read tl :: Int))]]
-      ('L':tl) -> list ++ [(i, y) | i <- [x-1..(x-(read tl :: Int))]]
+      ('L':tl) -> list ++ [(i, y) | i <- reverse [(x-(read tl :: Int))..x-1]]
       ('U':tl) -> list ++ [(x, j) | j <- [y+1..(y+(read tl :: Int))]]
-      ('D':tl) -> list ++ [(x, j) | j <- [y-1..(y-(read tl :: Int))]]
+      ('D':tl) -> list ++ [(x, j) | j <- reverse [(y-(read tl :: Int))..y-1]]
 
 endOfPath :: [(Int, Int)] -> (Int, Int)
 endOfPath [] = (0, 0)
 endOfPath list = last list
 
 getMinimumIntersection :: [[(Int, Int)]] -> (Int, Int)
-getMinimumIntersection (fst:snd:tl) = head $ sortBy manhattanOrdering $ intersect fst snd
+getMinimumIntersection (fst:snd:tl) = head $ sortBy manhattanOrdering $ Set.toList $ Set.intersection (Set.fromList fst) (Set.fromList snd)
 
 manhattanOrdering :: (Int, Int) -> (Int, Int) -> Ordering
 manhattanOrdering x y
