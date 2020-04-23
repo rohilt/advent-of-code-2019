840d84173fa57a5e4da9a194a11de215f812a7f6
Rohil Tuli <rohil.tuli@gmail.com>
2020-04-23 18:56:20 -0400

    Finished day 4

--- day4.hs
+++ day4.hs
@@ -5,7 +5,7 @@ main = do
    print $ length $ filter validPassword [240298..784956]
 
 validPassword :: Int -> Bool
-validPassword x = (twoConsecutive $ convertToDigits x) && (nonDecreasing $ convertToDigits x)
+validPassword x = (twoConsecutiveNew $ convertToDigits x) && (nonDecreasing $ convertToDigits x)
 
 convertToDigits :: Int -> [Int]
 convertToDigits i = map digitToInt $ show i
@@ -13,6 +13,9 @@ convertToDigits i = map digitToInt $ show i
 twoConsecutive :: [Int] -> Bool
 twoConsecutive (a:b:tl) = (a == b) || twoConsecutive (b:tl)
 twoConsecutive _ = False
+
+twoConsecutiveNew :: [Int] -> Bool
+twoConsecutiveNew x = or [(length $ filter (==i) x) == 2 | i <- [0..9]]
 
 nonDecreasing :: [Int] -> Bool
 nonDecreasing (a:b:tl) = (a <= b) && nonDecreasing (b:tl)
